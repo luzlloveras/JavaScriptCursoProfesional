014e16bab08652838395fa545a769c2f1ec0aa8d
luzlloveras <luzlloveras7@gmail.com>
2023-11-14 20:31:02 -0300

    pase por valor y referencia

--- m3paseValorReferencia.js
+++ m3paseValorReferencia.js
@@ -1,3 +1,5 @@
+// pase por referencia: el parámetro apunta a la misma dirección que el argumento original. Cuando modificamos el valor, se modifica el valor en el argumento original.
+// pase por valor: se envia una copia del argumento original. Esta copia está en una dirección distinta y el parámetro está apuntando a una dirección diferente. Cualquier modificación al parámetro no afecta al valor original.
 let edad = 20;
 
 function modificador(edad){
